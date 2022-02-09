244a7d735b49b55c6250aa421d7ccc8550ac3158
Martín <magadiflo@gmail.com>
2022-02-09 11:28:18 -0500

    Primeros componentes e inicio de la estructura del proyecto

---
+++ src/app/pages/not-page-found/not-page-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-page-found',
+  templateUrl: './not-page-found.component.html',
+  styles: [
+  ]
+})
+export class NotPageFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
