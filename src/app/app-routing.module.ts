6546306c18e01595abfd13cdc35a04681a503639
Martín <magadiflo@gmail.com>
2022-02-11 12:33:31 -0500

    AuthRoutingModule - Con esto finaliza la implementación de >>>>  Rutas hijas por módulo <<<<

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
 
-const routes: Routes = [  
-  { path: 'login', component: LoginComponent, },
-  { path: 'register', component: RegisterComponent, },
+const routes: Routes = [
   { path: '**', component: NotPageFoundComponent, },
 ];
 
@@ -17,6 +14,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     PagesRoutingModule,
+    AuthRoutingModule,
   ],
   exports: [
     RouterModule,
