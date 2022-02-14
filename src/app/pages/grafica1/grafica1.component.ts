932de4c9f5414e5402f8ad6bc5d8f78690dae3bc
Martín <magadiflo@gmail.com>
2022-02-14 13:19:10 -0500

    Gráficas en Angular

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { MultiDataSet, Label, Color } from "ng2-charts";
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [
+    `.contenedor {
+      display: blocK;
+    }`
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [[350, 450, 100]];
+  public colors: Color[] = [
+    { backgroundColor: ['#6857E6', '#009FEE', '#F02059'] }
+  ];
 
-  ngOnInit(): void {
-  }
+
 
 }
