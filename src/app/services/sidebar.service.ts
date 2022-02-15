211b82eacd03d5283833ea198c9d12d39c2ee408
Martín <magadiflo@gmail.com>
2022-02-15 12:09:36 -0500

    Servicio para controlar el Sidebar - Menú lateral

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+interface Menu {
+  titulo: string,
+  icono: string,
+  submenu: SubMenu[],
+}
+
+interface SubMenu {
+  titulo: string,
+  url: string,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: Menu[] = [
+    {
+      titulo: 'Principal',
+      icono: 'mdi mdi-hand-pointing-right',
+      submenu: [
+        { titulo: 'Main', url: '/' },
+        { titulo: 'ProgressBar', url: 'progress' },
+        { titulo: 'Gráficas', url: 'grafica1' },
+      ],
+    }
+  ];
+
+  constructor() { }
+}
