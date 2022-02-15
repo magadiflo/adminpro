8b08c26c751bbe261f661214fb8337ca292a9f5d
Martín <magadiflo@gmail.com>
2022-02-14 22:24:26 -0500

    Tarea de Inputs y Outputs

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
 
-import { MultiDataSet, Label, Color } from "ng2-charts";
+
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [
-    `.contenedor {
-      display: blocK;
-    }`
+    
   ]
 })
 export class Grafica1Component {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [[350, 450, 100]];
-  public colors: Color[] = [
-    { backgroundColor: ['#6857E6', '#009FEE', '#F02059'] }
-  ];
+  
 
 
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 
-import { ChartsModule } from 'ng2-charts';
-
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
@@ -30,7 +28,6 @@ import { PagesComponent } from './pages.component';
     CommonModule,
     FormsModule,
     RouterModule,
-    ChartsModule,
     SharedModule,
     ComponentsModule,
   ]
