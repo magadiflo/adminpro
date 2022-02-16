9327ef9e69819e21f0456c466b60ead40c467511
Martín <magadiflo@gmail.com>
2022-02-16 12:34:50 -0500

    Operador map de los observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Observable, retry } from 'rxjs';
+import { Observable, retry, interval, take, map } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -11,14 +11,25 @@ import { Observable, retry } from 'rxjs';
 export class RxjsComponent {
 
   constructor() {
-    this.retornaObservable()
+    // this.retornaObservable()
+    //   .pipe(
+    //     retry(1) //Captura el error cuando i = 2 y lo vuelve a intentar, pero ya i se incrementa a 3 por que está de manera global. (1) es el número de intentos a realizar
+    //   ).subscribe({
+    //     next: (valor) => console.log('Subs:', valor),
+    //     error: (error) => console.warn('Error:', error),
+    //     complete: () => console.info('Se completó...'),
+    //   });
+
+    this.retornaIntervalo()
+      .subscribe(console.log);
+  }
+
+  retornaIntervalo(): Observable<string> {
+    return interval(1000)
       .pipe(
-        retry(1) //Captura el error cuando i = 2 y lo vuelve a intentar, pero ya i se incrementa a 3 por que está de manera global. (1) es el número de intentos a realizar
-      ).subscribe({
-        next: (valor) => console.log('Subs:', valor),
-        error: (error) => console.warn('Error:', error),
-        complete: () => console.info('Se completó...'),
-      });
+        take(4), //cuántas emisiones del observable necesita. Es decir cuando llegue a 4 emisiones, finalizará
+        map(valor => `Cadena retornada ${valor + 1}`), //El map, transforma la información que recibe el observable y mutarla según yo lo necesite
+      );
   }
 
   retornaObservable(): Observable<number> {
