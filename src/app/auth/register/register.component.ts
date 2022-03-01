76e870cdcf960e3aa8db9f10ba32e33f8daa7586
Martín <magadiflo@gmail.com>
2022-02-28 23:07:52 -0500

    Validaciones de formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -10,18 +10,33 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   public miFormulario = this.fb.group({
-    nombre: ['Martín', [Validators.required, Validators.minLength(3)]],
-    email: ['tinkler@gmail.com', [Validators.required]],
-    password: ['123456', [Validators.required]],
-    password2: ['123456', [Validators.required]],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    password2: ['', [Validators.required]],
     terminos: [false, [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) { }
 
   crearUsuario(): void {
-    console.log(this.miFormulario.value);
+    this.formSubmitted = true;
+    if (this.miFormulario.valid && this.miFormulario.controls['terminos'].value) {
+      console.log('Posteando formulario...');
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+  }
+
+  campoNoValido(campo: string): boolean {
+    return this.miFormulario.get(campo)!.invalid && this.formSubmitted;
+  }
+
+  aceptaTerminos(): boolean {
+    return !this.miFormulario.get('terminos')!.value && this.formSubmitted;
   }
 
 }
