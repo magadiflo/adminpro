3adbd2a8ad801f1b031448a07289d4c9743496b9
Martín <magadiflo@gmail.com>
2022-03-05 12:58:17 -0500

    Sección 16. Crear el componente de u suario

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -12,6 +12,9 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { PerfilComponent } from './perfil/perfil.component';
 
+//Mantenimientos
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+
 const routes: Routes = [
     {
         path: 'dashboard',
@@ -25,6 +28,10 @@ const routes: Routes = [
             { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
             { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
             { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
+            
+            //Mantenimientos
+            { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuarios de aplicación' } },
+
         ],
     },
 ];
