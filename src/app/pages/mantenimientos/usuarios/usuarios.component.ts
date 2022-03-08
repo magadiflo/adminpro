aa2b92405178c6c22979a7146aac00c6582ffe50
Martín <magadiflo@gmail.com>
2022-03-07 22:40:22 -0500

    Paginar los usuarios

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -17,15 +17,30 @@ export class UsuariosComponent implements OnInit {
 
   totalUsuarios: number = 0;
   usuarios: Usuario[] = [];
+  desde: number = 0;
 
   constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
-    this.usuarioService.cargarUsuarios(0)
+    this.cargarUsuarios();
+  }
+  
+  cargarUsuarios() {
+    this.usuarioService.cargarUsuarios(this.desde)
       .subscribe(({ total, usuarios }) => {
         this.totalUsuarios = total;
         this.usuarios = usuarios;
       });
+  }
+
+  cambiarPagina(valor: number) {
+    this.desde += valor;
+    if(this.desde < 0){
+      this.desde = 0;
+    } else if(this.desde > this.totalUsuarios){
+      this.desde -= valor;
+    }
+    this.cargarUsuarios();
   }
 
 
